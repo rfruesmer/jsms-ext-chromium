ac3fdecd3f88c273b04679b54aa895be3a54582c
Reinhold Früsmer <rfruesmer@gmail.com>
2020-01-27 23:27:42 +0100

    Changed jsms to dev/peer dependency

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,9 @@ export default [
     // Browser-friendly UMD build
     {
         input: "index.js",
+        external: ["jsms", "@log4js-node/log4js-api"],
         output: {
-            name: "jsms-ext-chromium",
+            name: "jsms",
             file: pkg.browser,
             format: "umd"
         }
@@ -14,7 +15,7 @@ export default [
     // CommonJS (for Node) and ES module (for bundlers) build.
     {
         input: "index.js",
-        external: ["uuid"],
+        external: ["uuid", "jsms", "@log4js-node/log4js-api"],
         output: [
             { file: pkg.main, format: "cjs" },
             { file: pkg.module, format: "es" }
